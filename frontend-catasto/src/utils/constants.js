dc0fd5a226d926df4a9024d05b2be26ed47cddcf
Antonio O. <antonio.orfitelli1@gmail.com>
2026-02-19 20:26:15 +0100

    cambiato immigrazione

--- frontend-catasto/src/utils/constants.js
+++ frontend-catasto/src/utils/constants.js
@@ -9,9 +9,15 @@ export const bestiameOptions = [
 
 export const immigrazioneOptions = [
   { id: 0, label: "Non specificato" },
-  { id: 1, label: "Cittadino" },
-  { id: 2, label: "Forestiero" },
-  { id: 3, label: "Abitante del Contado" },
+  { id: 1, label: "Proveniente da una località del territorio fiorentino" },
+  { id: 2, label: "Vive e risiede in una località del territorio fiorentino diversa da quella in cui è soggetto al pagamento" },
+  { id: 3, label: "Proveniente da una località italiana non soggetta a Firenze" },
+  { id: 4, label: "Proveniente dalla Germania" },
+  { id: 5, label: "Proveniente da un altro paese che non sia nè Italia nè Germania" },
+  { id: 6, label: "Residente in Italia ma al di fuori della Toscana fiorentina" },
+  { id: 7, label: "Residente in Germania" },
+  { id: 8, label: "Residente in un altro paese che non sia nè l'Italia nè la Germania" },
+  { id: 9, label: "Forestiero che possiede beni nel territorio fiorentino ma vive altrove" },
 ];
 
 export const rapportoOptions = [
